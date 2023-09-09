a34f076ae515675ba4a7b22e13945afc28d1492e
Jayamal Sigera <veelochanas@gmail.com>
2023-09-09 13:03:08 +0530

    update backend

--- backend/src/constants/httpStatus.js
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-export const BAD_REQUEST = 400;
-export const UNAUTHORIZED = 401;
-=======
-export const BAD_REQUEST = 400;
-export const UNAUTHORIZED = 401;
->>>>>>> bd25c73570701b2ccdd5741efa034975dd7f58c8
